5f3d29b417cd578c92fbdcc575961dc7e51272ee
Raza <razabinashraf@gmail.com>
2021-05-24 18:01:45 +0530

    changed debug mode to false

--- BFH_Venue/settings.py
+++ BFH_Venue/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pf+=w7*k1^dafx1pooc1x0g%!$+l0tx-nn$z6c38%$r+227gxh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
